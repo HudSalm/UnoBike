0f78a0e32584de8e7dc971364f85779c5564e524
Hudson <hudson.salm2001@outlook.com>
2026-01-02 13:28:51 -0300

    Adicionei a opção de abrir a loja da apple ou da google ao clicar no botão BAIXE O APP

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,6 +8,22 @@ import { Sling } from 'hamburger-react'
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const storeButtonClick = () => {
+        const userAgent = navigator.userAgent;
+        const androidUrl = "https://play.google.com/store/apps/details?id=com.unobike.bikedagenteguapi&utm_source=latam_Med";
+        const iosUrl = "https://apps.apple.com/br/app/bike-da-gente-guapi/id6751255440";
+
+        if (/android/i.test(userAgent)) {
+            window.location.href = androidUrl;
+        }
+        else if (/ipad|iphone|ipod/i.test(userAgent) && !(window as any).MSStream) {
+            window.location.href = iosUrl;
+        }
+        else {
+            window.location.href = androidUrl;
+        }
+    }
     
     return (
         <header className={styles.header}>
@@ -27,7 +43,7 @@ const Header = () => {
                     <li><Link to='/'>HOME</Link></li>
                     <li><Link to='/Help'>AJUDA</Link></li>
                     <li><Link to='/PrivacyPolicy'>POLÍTICA DE PRIVACIDADE</Link></li>
-                    <li><a className={styles.app} href="https://play.google.com/store/apps/details?id=com.unobike.bikedagenteguapi" target='_blank'>BAIXE O APP</a></li>
+                    <li className={styles.appButton} onClick={storeButtonClick}>BAIXE O APP</li>
                 </ul>
             </nav>
         </header>
